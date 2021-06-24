c2cf475ab87d723f2dd5c148ca99b58d0a3e1ac7
bruno <brmartins92@gmail.com>
2021-06-24 11:57:49 -0300

    bug cors

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 app.use(express.json());
 const port = 3000;
 
-
+app.use(cors());
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
+   
     next();
 });
 /* routers*/
